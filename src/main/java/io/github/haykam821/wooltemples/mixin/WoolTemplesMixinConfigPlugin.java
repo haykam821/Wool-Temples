7c2a941e2e01b1585be8f96c1c53276028fe6418
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-11-13 20:09:06 -0500

    Avoid using deprecated Fabric loader internal APIs

--- src/main/java/io/github/haykam821/wooltemples/mixin/WoolTemplesMixinConfigPlugin.java
+++ src/main/java/io/github/haykam821/wooltemples/mixin/WoolTemplesMixinConfigPlugin.java
@@ -13,17 +13,17 @@ import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
+import net.fabricmc.loader.api.SemanticVersion;
 import net.fabricmc.loader.api.Version;
 import net.fabricmc.loader.api.VersionParsingException;
-import net.fabricmc.loader.util.version.SemanticVersionImpl;
-import net.fabricmc.loader.util.version.SemanticVersionPredicateParser;
+import net.fabricmc.loader.api.metadata.version.VersionPredicate;
 
 public class WoolTemplesMixinConfigPlugin implements IMixinConfigPlugin {
 	private static final String MIXIN_CLASS_PREFIX = "io.github.haykam821.wooltemples.mixin.";
 	private static final String MIXIN_CLASS_1_15 = MIXIN_CLASS_PREFIX + "DesertTempleGeneratorMixin115";
 	private static final String MIXIN_CLASS_1_16 = MIXIN_CLASS_PREFIX + "DesertTempleGeneratorMixin116";
 
-	private static final Predicate<SemanticVersionImpl> IS_1_16 = createVersionCompatibility(">=1.16-alpha.20.13.a");
+	private static final Predicate<Version> IS_1_16 = createVersionCompatibility(">=1.16-alpha.20.13.a");
 
 	@Override
 	public void onLoad(String mixinPackage) {
@@ -66,22 +66,22 @@ public class WoolTemplesMixinConfigPlugin implements IMixinConfigPlugin {
 		return;
 	}
 
-	private static SemanticVersionImpl getMinecraftVersion() {
+	private static Version getMinecraftVersion() {
 		Optional<ModContainer> container = FabricLoader.getInstance().getModContainer("minecraft");
 
 		if (container.isPresent()) {
 			Version version = container.get().getMetadata().getVersion();
-			if (version instanceof SemanticVersionImpl) {
-				return (SemanticVersionImpl) version;
+			if (version instanceof SemanticVersion) {
+				return version;
 			}
 		}
 
 		return null;
 	}
 
-	private static Predicate<SemanticVersionImpl> createVersionCompatibility(String versionRange) {
+	private static Predicate<Version> createVersionCompatibility(String versionRange) {
 		try {
-			return SemanticVersionPredicateParser.create(versionRange);
+			return VersionPredicate.parse(versionRange);
 		} catch (VersionParsingException exception) {
 			return Predicates.alwaysFalse();
 		}
